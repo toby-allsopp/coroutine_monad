354fcfa60e2e689731f0c03600b29d7549e9da1b
Toby Allsopp <toby@mi6.gen.nz>
2017-06-10 22:22:41 +1200

    Rearrange the tests

---
+++ test_expected.cpp
@@ -0,0 +1,49 @@
+#include "either.h"
+
+#include "catch.hpp"
+
+struct error {
+  int code;
+};
+
+expected<int, error> f1() noexcept { return 7; }
+expected<double, error> f2(int x) noexcept { return 2.0 * x; }
+expected<int, error> f3(int x, double y) noexcept { return /*int(x + y)*/ error{42}; }
+
+expected<int, error> test_expected_manual() {
+  auto x = f1();
+  if (!x) return x.error();
+  auto y = f2(x.value());
+  if (!y) return y.error();
+  auto z = f3(x.value(), y.value());
+  return z;
+}
+
+inline auto pair_with = [](auto&& x) {
+  return [x](auto&& y) { return std::pair{std::move(x), std::forward<decltype(y)>(y)}; };
+};
+
+expected<int, error> test_expected_then() {
+  // clang-format off
+  auto z =
+      f1()
+      .then([](auto x) { return f2(x).transform(pair_with(x)); })
+      .then([](auto p) { auto[x, y] = p; return f3(x, y); });
+  // clang-format on
+  return z;
+}
+
+auto test_expected_coroutine() {
+  return []() -> expected<int, error> {
+    auto x = co_await f1();
+    auto y = co_await f2(x);
+    auto z = co_await f3(x, y);
+    co_return z;
+  }();
+}
+
+TEST_CASE("expected") {
+  auto r = test_expected_coroutine();
+  REQUIRE(!r.good());
+  REQUIRE(r.error().code == 42);
+}
